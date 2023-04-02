425cbab64abfb0c26a25f17b481cf8d2d589f3ea
SaynabHareed <123257294+SaynabHareed@users.noreply.github.com>
2023-04-02 13:53:52 +0100

    updated the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,29 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours} : ${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
 
@@ -17,7 +43,7 @@ function displayTemperature(response) {
   windElement.innerHTML = response.data.wind.degree;
 
   let dateElement = document.querySelector("#date");
-  dateElement.innerHTML = response.data.time;
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
 }
 let apiKey = "875a2tbd2fbb2b16oab443ef3f004a3c";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=London&key=${apiKey}&units=metric`;
