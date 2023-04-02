093abc011b2c7a7424dab96024764a820232e32f
SaynabHareed <123257294+SaynabHareed@users.noreply.github.com>
2023-04-02 13:11:15 +0100

    showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
+function displayTemperature(response) {
+  console.log(response.data);
 
+  let degreeElement = document.querySelector("#degree");
+  degreeElement.innerHTML = Math.round(response.data.temperature.current);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = response.data.wind.degree;
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = response.data.time;
+}
+let apiKey = "875a2tbd2fbb2b16oab443ef3f004a3c";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=London&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
